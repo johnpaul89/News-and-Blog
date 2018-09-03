07e9fe712326e6cf3e1fd06bcb5f9fdb4e5c0448
JohnPaul Gitata <johnpaulgitata@gmail.com>
2018-09-03 19:13:39 +0300

    Create Authentication of Sign Up

--- news_and_blog/urls.py
+++ news_and_blog/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('tuk.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
